3e896a72817ff923cb5bff2320c9a4e89e8ddf95
ongaeshi <ongaeshi0621@gmail.com>
2014-02-17 23:26:19 +0900

    Add test.pdf

--- test/test_pdf.rb
+++ test/test_pdf.rb
@@ -1,13 +1,25 @@
 require 'minitest_helper'
 
 class TestPdf < MiniTest::Test
+  def test_pdf
+    File.join(File.dirname(__FILE__), "test.pdf")
+  end
+  
   def test_pages
-    pages = Honyomi::Pdf.new("~/tmp/matz.pdf")
-    p pages.pages.map { |page| page[0..10] }
+    pdf = Honyomi::Pdf.new(test_pdf)
+
+    assert_equal         3, pdf.pages.size
+    assert_equal "aaa\n\n", pdf.pages[0]
+    assert_equal "bbb\n\n", pdf.pages[1]
+    assert_equal "ccc\n\n", pdf.pages[2]
   end
 
   def test_strip_pages
-    pages = Honyomi::Pdf.new("~/tmp/matz.pdf")
-    p pages.strip_pages.map { |page| page[0..10] }
+    pdf = Honyomi::Pdf.new(test_pdf)
+    pages = pdf.strip_pages
+
+    assert_equal "aaa", pages[0]
+    assert_equal "bbb", pages[1]
+    assert_equal "ccc", pages[2]
   end
 end
