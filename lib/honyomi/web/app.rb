84ea1af248cf395c3ab8501354add4aa26ff75f1
ongaeshi <ongaeshi0621@gmail.com>
2014-03-02 00:11:46 +0900

    Add <pre>result</pre>

--- lib/honyomi/web/app.rb
+++ lib/honyomi/web/app.rb
@@ -13,6 +13,26 @@ end
 
 get '/' do
   @database = $database
+
+  results = @database.search("css text")
+  results.sort([["_score", :desc]])
+  snippet = GrnMini::Util::html_snippet_from_selection_results(results)
+
+  r = results.map do |page|
+    text = "--- #{page.book.title} (#{page.page_no} page) ---\n"
+    snippet.execute(page.text).each do |segment|
+      text += segment.gsub("\n", "") + "\n"
+    end
+    text
+  end
+
+  @content = <<EOF
+<pre>
+#{results.size} matches
+#{r.join("\n\n")}
+</pre>
+EOF
+
   haml :index
 end
 
