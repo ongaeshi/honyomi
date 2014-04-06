5e7643f88580d048ef7bbdb1d8ed282d13f114f5
ongaeshi <ongaeshi0621@gmail.com>
2014-04-06 14:23:59 +0900

    Set timestamp column on 'honyomi add'

--- lib/honyomi/core.rb
+++ lib/honyomi/core.rb
@@ -24,8 +24,10 @@ module Honyomi
 
     def add(filename, options = {})
       if File.exist?(filename)
+        options = options.dup
         pages = Pdf.new(filename).pages
         pages = pages.map { |page| Util.strip_page(page) } if options[:strip]
+        options[:timestamp] = File.stat(filename).mtime
         @database.add_book(filename, pages, options)
       else
         nil
