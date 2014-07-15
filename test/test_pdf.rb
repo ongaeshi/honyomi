c1dc04a38aa6c073c9c49a7e2e424c2afcb5a334
labocho <labocho@penguinlab.jp>
2014-07-15 13:48:48 +0900

    Support file name includes parenthesis or any special characters

--- test/test_pdf.rb
+++ test/test_pdf.rb
@@ -4,7 +4,7 @@ class TestPdf < MiniTest::Test
   def test_pdf
     File.join(File.dirname(__FILE__), "test.pdf")
   end
-  
+
   def test_pages
     pdf = Honyomi::Pdf.new(test_pdf)
 
@@ -12,5 +12,16 @@ class TestPdf < MiniTest::Test
     assert_equal "aaa\n\n", pdf.pages[0]
     assert_equal "bbb\n\n", pdf.pages[1]
     assert_equal "ccc\n\n", pdf.pages[2]
+  end
+
+  def test_file_name_includes_special_characters
+    Dir.mktmpdir do |dir|
+      filename = File.join(dir, "test (1).pdf")
+
+      FileUtils.cp test_pdf, filename
+      pdf = Honyomi::Pdf.new(filename)
+
+      assert_equal 3, pdf.pages.size
+    end
   end
 end
