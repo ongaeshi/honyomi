d068199e39612e1f96336d0d8401823ebfa6eb97
ongaeshi <ongaeshi0621@gmail.com>
2014-10-07 00:52:49 +0900

    Add Util.highlight_keywords

--- lib/honyomi/util.rb
+++ lib/honyomi/util.rb
@@ -37,5 +37,59 @@ module Honyomi
     def strip_page(page)
       page.gsub(/[ \t]/, "")
     end
+
+    def highlight_keywords(src, keywords, css_class)
+      # Init highlight_map
+      hightlight_map = Array.new(src.length, nil)
+
+      keywords.each do |keyword|
+        pos = 0
+
+        loop do
+          r = src.match(/#{Regexp.escape(keyword)}/i, pos) do |m|
+            s = m.begin(0)
+            l = keyword.length
+            e = s+l
+            (s...e).each {|i| hightlight_map[i] = 1 }
+            pos = e
+          end
+
+          break if r.nil?
+        end
+      end
+
+      # Delete html tag
+      index = 0
+      in_tag = false
+      src.each_char do |char|
+        in_tag = true               if char == '<'
+        hightlight_map[index] = nil if in_tag
+        in_tag = false              if char == '>'
+        index += 1
+      end
+
+      # Output
+      result = ""
+
+      index = 0
+      prev = nil
+      src.each_char do |char|
+        current = hightlight_map[index]
+
+        if prev.nil? && current
+          result += "<span class='#{css_class}'>"
+        elsif prev && current.nil?
+          result += "</span>"
+        end
+
+        result += char
+
+        index += 1
+        prev = current
+      end
+      result += "</span>" if prev
+
+      result
+    end
   end
 end
