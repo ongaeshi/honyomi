b444964e0b80246ac760a6c2bd0679c986eb0cde
ongaeshi <ongaeshi0621@gmail.com>
2014-11-12 01:07:47 +0900

    Set weight to Bookmarks#comment

--- lib/honyomi/database.rb
+++ lib/honyomi/database.rb
@@ -100,7 +100,12 @@ module Honyomi
       match_pages = @pages.select(query.page_query, default_column: "text")
       snippet = match_pages.expression.snippet([["<span class=\"highlight\">", "</span>"]], {html_escape: true, normalize: true, max_results: 5})
 
-      match_bookmarks = @bookmarks.select(query.bookmark_query, default_column: "comment")
+      match_bookmarks = @bookmarks.select do |record|
+        record.match(query.bookmark_query) do |target|
+          target.comment * 10
+        end
+      end
+
       group_by_page = match_bookmarks.group("page")
 
       return group_by_page.union!(match_pages), snippet
