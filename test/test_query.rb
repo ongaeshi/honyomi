7cebb963336127e30e944d54233891ff18572e4e
ongaeshi <ongaeshi0621@gmail.com>
2014-11-12 01:40:31 +0900

    Replace string from query to page_query

--- test/test_query.rb
+++ test/test_query.rb
@@ -5,53 +5,53 @@ module Honyomi
   class TestQuery < MiniTest::Test
     def test_simple
       q = Query.new("aaa")
-      assert_equal "aaa", q.query
+      assert_equal "aaa", q.page_query
       assert_equal nil, q.jump_page_no
     end
 
     def test_jump_page_no
       q = Query.new("111")
-      assert_equal "page_no:111", q.query
+      assert_equal "page_no:111", q.page_query
       assert_equal 111, q.jump_page_no
       
       q = Query.new("111 123")
-      assert_equal "page_no:111 page_no:123", q.query
+      assert_equal "page_no:111 page_no:123", q.page_query
       assert_equal 123, q.jump_page_no
 
       q = Query.new("aaa 222")
-      assert_equal "aaa page_no:222", q.query
+      assert_equal "aaa page_no:222", q.page_query
       assert_equal 222, q.jump_page_no
     end
 
     def test_phrase
       q = Query.new("aaa \"bbb ccc\" ddd")
-      assert_equal "aaa \"bbb ccc\" ddd", q.query
+      assert_equal "aaa \"bbb ccc\" ddd", q.page_query
     end
 
     def test_book
       q = Query.new("b:22 bbb")
-      assert_equal "book:22 bbb", q.query
+      assert_equal "book:22 bbb", q.page_query
     end
 
     def test_book_title
       q = Query.new("t:\"aaa bbb\" ccc")
-      assert_equal "book.title:@\"aaa bbb\" ccc", q.query
+      assert_equal "book.title:@\"aaa bbb\" ccc", q.page_query
     end
 
     def test_page_no
       q = Query.new("p:11 t:\"aaa bbb\"")
-      assert_equal "page_no:11 book.title:@\"aaa bbb\"", q.query
+      assert_equal "page_no:11 book.title:@\"aaa bbb\"", q.page_query
     end
 
     def test_and_or
       q = Query.new("aaa + bbb")
-      assert_equal "aaa + bbb", q.query
+      assert_equal "aaa + bbb", q.page_query
 
       q = Query.new("aaa OR bbb")
-      assert_equal "aaa OR bbb", q.query
+      assert_equal "aaa OR bbb", q.page_query
 
       q = Query.new("aaa (\"ddd bbb\" OR t:CCC)")
-      assert_equal "aaa (\"ddd bbb\" OR book.title:@CCC)", q.query
+      assert_equal "aaa (\"ddd bbb\" OR book.title:@CCC)", q.page_query
     end
 
     def test_bookmark_query
