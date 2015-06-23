d7c82676fbea3b198ac94f5c7531bb3f8ae36073
ongaeshi <ongaeshi0621@gmail.com>
2015-06-23 23:48:42 +0900

    Add scrub

--- lib/honyomi/pdf.rb
+++ lib/honyomi/pdf.rb
@@ -10,6 +10,9 @@ module Honyomi
         outfile = File.join(dir, "pdf.txt")
         system("pdftotext", filename, outfile) # Need pdftotext (poppler, xpdf)
         @text = File.read(outfile, encoding: Encoding::UTF_8)
+        if String.method_defined? :scrub
+          @text = @text.scrub('?')
+        end
       end
     end
 
