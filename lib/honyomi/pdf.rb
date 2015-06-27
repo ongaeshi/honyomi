2c10ad5cd9606a7dcff27ba8217e1295a6637263
ongaeshi <ongaeshi0621@gmail.com>
2015-06-27 11:03:52 +0900

    Move a call pdftotext from initialize to pages

--- lib/honyomi/pdf.rb
+++ lib/honyomi/pdf.rb
@@ -5,18 +5,18 @@ module Honyomi
   class Pdf
     def initialize(filename)
       @filename = filename
+    end
 
+    def pages
       Dir.mktmpdir do |dir|
         outfile = File.join(dir, "pdf.txt")
-        system("pdftotext", filename, outfile) # Need pdftotext (poppler, xpdf)
+        system("pdftotext", @filename, outfile) # Need pdftotext (poppler, xpdf)
         @text = File.read(outfile, encoding: Encoding::UTF_8)
         if String.method_defined? :scrub
           @text = @text.scrub('?')
         end
       end
-    end
 
-    def pages
       @text.split("\f")
     end
   end
