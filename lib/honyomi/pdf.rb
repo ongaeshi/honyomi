820d093c1b8adb6d61da7bab9debec0686020fff
ongaeshi <ongaeshi0621@gmail.com>
2015-11-01 22:10:41 +0900

    Fix page parse bug

--- lib/honyomi/pdf.rb
+++ lib/honyomi/pdf.rb
@@ -10,16 +10,28 @@ module Honyomi
     end
 
     def pages
+      result = []
+      
       Dir.mktmpdir do |dir|
         outfile = File.join(dir, "pdf.txt")
-        system("pdftotext", @filename, outfile) # Need pdftotext (poppler, xpdf)
-        @text = File.read(outfile, encoding: Encoding::UTF_8)
-        if String.method_defined? :scrub
-          @text = @text.scrub('?')
+
+        loop do
+          page_no = (result.count + 1).to_s
+
+          is_success = system("pdftotext", "-f", page_no, "-l", page_no, @filename, outfile) # Need pdftotext (poppler, xpdf)
+          break unless is_success
+          
+          text = File.read(outfile, encoding: Encoding::UTF_8)
+
+          if String.method_defined? :scrub
+            text = text.scrub('?')
+          end
+
+          result << text
         end
       end
 
-      @text.split("\f")
+      result
     end
 
     def generate_images(output_dir)
